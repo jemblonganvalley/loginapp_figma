28d98e60b10090847055109b9e57b8b725904069
jemblonganvalley <jemblonganvalley@gmail.com>
2021-01-11 21:22:43 +0700

    pull coy

--- src/components/loginform/LoginForm.jsx
+++ src/components/loginform/LoginForm.jsx
@@ -9,7 +9,29 @@ const LoginForm = () => {
         </div>
 
         <div className="right_side">
-          <h1>right side</h1>
+          <h1>Sign In</h1>
+
+          <form className="login_form">
+            <input
+              type="text"
+              id="email"
+              name="email"
+              placeholder="Email Address"
+            />
+            <input
+              type="password"
+              id="password"
+              name="password"
+              placeholder="password"
+            />
+            <button className="login_button">continue</button>
+          </form>
+
+          <div className="socmed_login">
+            <h3>Or Login With</h3>
+            <button className="g_login">Sign In With Google</button>
+            <button className="f_login">Sign in Wit Facebook</button>
+          </div>
         </div>
       </div>
     </main>
